d3cdf418560cf46526c56ae77bf7db3c57e11177
Jorge.acruz <jorge.cruz@crmservives.com.br>
2022-10-27 17:53:40 -0300

    Tour Agency app

---
+++ Tour-Agency/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Routes from './src/routes';
+export default function App() {
+ return (
+      <Routes/>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center'
+  }
+})
