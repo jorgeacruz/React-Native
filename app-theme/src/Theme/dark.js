306cdea1deea3ec5b34b7179829a87c10b999ce5
Jorge.acruz <jorge.cruz@crmservives.com.br>
2023-05-03 18:01:23 -0300

    Delete App theme

--- app-theme/src/Theme/dark.js
+++
@@ -1,6 +0,0 @@
-export default {
-    background:'#552583',
-    color:'#fff',
-    tag:'#fff',
-    titulo:'#fff'
-}
--- app-theme/src/Theme/light.js
+++
@@ -1,6 +0,0 @@
-export default {
-    background:'#FDB927',
-    color:'#000',
-    tag:'#fff',
-    titulo:'#552583'
-}
