0a294d25d4355eb677b9b508bf8005d00db01208
Jorge Cruz <j.alexcruz@gmail.com>
2023-12-14 21:12:45 -0300

    Others settings

---
+++ search-app/src/components/data.js
@@ -0,0 +1,45 @@
+export default [
+    {
+      id: 1,
+      name: 'Beatriz Farias',
+      email: 'bia.farias@gmail.com',
+      avatar: 'https://i.pravatar.cc/150?img=31',
+    },
+    {
+      id: 2,
+      name: 'Julia Santos',
+      email: 'jujusantos123@hotmail.com',
+      avatar: 'https://i.pravatar.cc/150?img=32',
+    },
+    {
+      id: 3,
+      name: 'Pedro Mendonça',
+      email: 'eu@pedro.com',
+      avatar: 'https://i.pinimg.com/564x/ca/b1/59/cab1597ecf120baab902dff8c69746c7.jpg',
+    },
+    {
+      id: 4,
+      name: 'Julia Maria',
+      email: 'julia.shinoda@japao.com',
+      avatar: 'https://i.pravatar.cc/150?img=34',
+    },
+    {
+      id: 5,
+      name: 'Andrezza Shinoda',
+      email: 'andrezza99@hotmail.com',
+      avatar: 'https://i.pravatar.cc/150?img=35',
+    },
+    {
+      id: 6,
+      name: 'Sara Maria',
+      email: 'saramaria@gmail.com',
+      avatar: 'https://i.pravatar.cc/150?img=36',
+    },
+    {
+      id: 7,
+      name: 'Edna Sousa',
+      email: 'edna21@yahoo.com.br',
+      avatar: 'https://i.pravatar.cc/150?img=37',
+    },
+  ];
+  
