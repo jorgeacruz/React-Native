f8a5eb4ff58556f1e107911e4caaed8906848bd7
Jorge Cruz <j.alexcruz@gmail.com>
2023-12-14 21:08:56 -0300

    build all styles App.js

--- search-app/App.js
+++ search-app/App.js
@@ -1,20 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, ImageBackground, TextInput, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="bg-primary flex-1 items-center pt-20">
+      <ImageBackground source={{uri:'https://i.pinimg.com/564x/2d/f1/60/2df160ce5ef84f3f125fa6d4ee540a32.jpg'}}
+        className="w-[530px] h-screen absolute opacity-30" />
+      <View>
+        <TextInput
+        placeholder='Searching for the bests players of Buck'
+        placeholderTextColor={'#fff'}
+        className="w-[320px] h-[40px] border border-white rounded-md text-white text-center mt-10" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
